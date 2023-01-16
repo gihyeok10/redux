8b97d547e8fee0fe5b6b9f0687b189eb7b9e87e4
kimgihyeok <gh9812301@naver.com>
2023-01-16 14:55:46 +0900

    redux practice

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,12 +1,13 @@
-import { useState } from 'react';
+import { useDispatch,useSelector } from 'react-redux';
 import './App.css';
 //redux : 상태관리가 가능한 라이버러리 // 저장소.
 //useDispatch ==> 컴포넌트가 던지는 액션
 //useSelector ==> 컴포넌트 store에 있는 값을 가져와서 쓸때 사용. 
 function App() {
-  const [counter,setCounter] =useState(0);
+  const counter = useSelector(state => state.counter)
+  const disPatch = useDispatch()
   const increase = () => {
-    setCounter(counter+1)
+    disPatch({type:"INCREMENT"}) //무조건 객체 안에 type == 액션의 이름
   }
   return (
     <div>
--- redux/src/redux/reducer/reducer.js
+++ redux/src/redux/reducer/reducer.js
@@ -1,3 +1,4 @@
+
 let initialState = {
     counter:0
 }
@@ -5,6 +6,18 @@ let initialState = {
 
 function reducer(state=initialState,action){
 
+    console.log("액션은몰까",action)
+    // if(action.type === "INCREMENT"){
+    //     return {...state,counter: state.counter +1} //reducer가 새로운값을 던저주면 return으로 적용! ...한 이유는 여러개중에 counter만 바꿀라고
+
+    // }
+
+    switch(action.type){
+        case "INCREMENT":
+        return {...state, counter: state.counter +1}
+        default:
+        return {...state};
+    }
 
 }
 
